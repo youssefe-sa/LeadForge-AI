6178888b66b87d5f50ea743989881fc29a79f263
sahab youssef <sahabyoussef@gmail.com>
2026-04-04 19:57:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss()], // viteSingleFile() désactivé temporairement
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
